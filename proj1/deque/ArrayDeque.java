df6cafdd61e235988628634aa687ab37a7039e8e
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-01 00:02:30 +0800

    feat: array deque

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,128 @@
+package deque;
+
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements IDeque<T> {
+    private class DequeIterator implements Iterator<T> {
+        private int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            T value = get(index);
+            index++;
+            return value;
+        }
+    }
+
+    private int size;
+    private T[] items;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        nextFirst = 4;
+        nextLast = 5;
+        size = 0;
+    }
+
+    public Iterator<T> iterator() {
+        return new DequeIterator();
+    }
+
+    private void resize(int newSize) {
+        assert size <= newSize;
+        T[] newArr = (T[]) new Object[newSize];
+        int newNextFirst = newSize / 2;
+        for (int i = 1; i <= size; i++) {
+            int newIndex = newNextFirst + i;
+            newIndex = newIndex < newSize ? newIndex : newIndex - newSize;
+            int index = nextFirst + i;
+            index = index < items.length ? index : index - items.length;
+            newArr[newIndex] = items[index];
+        }
+        nextFirst = newNextFirst;
+        nextLast = nextFirst + size + 1;
+        nextLast = nextLast < newSize ? nextLast : nextLast - newSize;
+        items = newArr;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (nextFirst == nextLast) {
+            resize(items.length * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = nextFirst == 0 ? items.length - 1 : nextFirst - 1;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (nextFirst == nextLast) {
+            resize(items.length * 2);
+        }
+        items[nextLast] = item;
+        nextLast = nextLast + 1 == items.length ? 0 : nextLast + 1;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 1; i <= size; i++) {
+            int index = nextFirst + i;
+            index = index < items.length ? index : index - items.length;
+            sb.append(items[index]);
+            sb.append(", ");
+        }
+        sb.append("}");
+        System.out.println(sb);
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size >= 16 && size / (double) items.length < 0.25) {
+            resize(size * 2);
+        }
+        T oldFirst = items[nextFirst + 1];
+        nextFirst = nextFirst == items.length - 1 ? 0 : nextFirst + 1;
+        size--;
+        return oldFirst;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size >= 16 && size / (double) items.length < 0.25) {
+            resize(size * 2);
+        }
+        T oldLast = items[nextLast - 1];
+        nextLast = nextLast == 1 ? items.length - 1 : nextLast - 1;
+        size--;
+        return oldLast;
+    }
+
+    @Override
+    public T get(int index) {
+        int i = nextFirst + 1 + index;
+        i = i >= items.length ? i - items.length : i;
+        return items[i];
+    }
+}
---
+++ proj1/deque/ArrayDequeTest.java
@@ -0,0 +1,80 @@
+package deque;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class ArrayDequeTest {
+    @Test
+    public void createEmptyDeque() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        assertEquals(0, d.size());
+        assertTrue(d.isEmpty());
+    }
+
+    @Test
+    public void addItem() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        d.addLast(100);
+        d.addLast(200);
+        assertEquals(2, d.size());
+    }
+
+    @Test
+    public void removeItem() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        d.addLast(100);
+        d.addLast(200);
+        assertEquals(100, (int) d.removeFirst());
+        assertEquals(d.size(), 1);
+        d.addFirst(100);
+        assertEquals(200, (int) d.removeLast());
+        assertEquals(d.size(), 1);
+    }
+
+    @Test
+    public void resizeTest() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            d.addFirst(5);
+            d.addLast(6);
+        }
+        assertEquals(20, d.size());
+        for (int i = 0; i < 10; i++) {
+            d.removeFirst();
+        }
+        assertEquals(10, d.size());
+        for (int i = 0; i < 5; i++) {
+            d.removeLast();
+        }
+        assertEquals(5, d.size());
+    }
+
+    @Test
+    public void getTest() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            d.addFirst(i);
+            d.addLast(i);
+        }
+        assertEquals(9, (int) d.get(0));
+        assertEquals(7, (int) d.get(2));
+        assertEquals(0, (int) d.get(9));
+    }
+
+    @Test
+    public void iteratorTest() {
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        Iterator<Integer> it = d.iterator();
+        for (int i = 0; i < 10; i++) {
+            d.addFirst(i);
+            d.addLast(i);
+        }
+        for (int i = 0; i < 20; i++) {
+            assertEquals(it.next(), d.get(i));
+        }
+        assertFalse(it.hasNext());
+    }
+}
