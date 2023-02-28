d78ea0c967d508e416a867d900de172276f86c71
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-28 11:11:51 +0800

    feat: comparable and comparator

--- playground/Dog.java
+++ playground/Dog.java
@@ -1,4 +1,13 @@
-public class Dog implements MyComparable {
+import java.util.Comparator;
+
+public class Dog implements Comparable<Dog> {
+    private static class NameComparator implements Comparator<Dog> {
+        @Override
+        public int compare(Dog a, Dog b) {
+            return a.name.compareTo(b.name);
+        }
+    }
+
     private String name;
     private int size;
 
@@ -7,19 +16,34 @@ public class Dog implements MyComparable {
         this.size = size;
     }
 
-    public int compareTo(Object o) {
-        return this.size - ((Dog) o).size;
+    public int compareTo(Dog dog) {
+        return this.size - dog.size;
     }
 
     public int size() {
         return size;
     }
 
+    public static Comparator<Dog> getNameComparator() {
+        return new NameComparator();
+    }
+
     public static void main(String[] args) {
+        // use comparable
         Dog[] dogs = new Dog[]{new Dog("Kelvin", 10),
                 new Dog("Lydia", 20),
                 new Dog("Jack", 5)};
         Dog biggest = (Dog) Maximizer.max(dogs);
         System.out.println(biggest.size());
+
+        // use comparator
+        Comparator<Dog> nc = Dog.getNameComparator();
+        Dog d1 = new Dog("Kelvin", 100);
+        Dog d2 = new Dog("Lydia", 200);
+        if (nc.compare(d1, d2) > 0) {
+            System.out.println(d1.name);
+        } else {
+            System.out.println(d2.name);
+        }
     }
 }
--- playground/Maximizer.java
+++ playground/Maximizer.java
@@ -1,5 +1,5 @@
 public class Maximizer {
-    public static MyComparable max(MyComparable[] items) {
+    public static Comparable max(Comparable[] items) {
         int maxIndex = 0;
         for (int i = 0; i < items.length; i++) {
             if (items[i].compareTo(items[maxIndex]) > 0) {
